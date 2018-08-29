7b382ede09ff4b9910f19d93161b3cda58ab5dad
OStefani <ostefanishina@gmail.com>
2018-08-29 18:44:59 +0300

    Add list of products

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route } from 'react-router';
+import { HashRouter } from 'react-router-dom';
+import Products from './Product.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <HashRouter>
+        <div>
+        <Route path="/" component={App}/>
+    {/* add the routes here */}
+        <Route path="/1" component={Products}/>
+        <Route path="/2" component={Products}/>
+        </div>
+        
+    </HashRouter>, document.getElementById('root'));
+
