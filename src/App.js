8714d6609b82bf0c642ccacf2cdc210af4593629
OStefani <ostefanishina@gmail.com>
2018-08-29 19:15:43 +0300

    Remove react router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 //import RenderItems from './Product.js'
-import { Router, Route, hashHistory } from 'react-router'
-import { Link } from 'react-router-dom'
-
-class ListOfProducts extends Component{
-  constructor (props) {
-    super(props);
-    this.state = {
-      token: null,
-      response: null
-    }
-  } 
-  componentDidMount() {
-    fetch('http://smktesting.herokuapp.com/api/products/')
-    .then(result => {
-      if(result.ok) {
-        return result.json();
-      }
-      else {
-        return Promise.reject(result.status);
-      }
-    }).then(result => {
-      this.setState({response: result}, () => {
-        return result;
-      });
-    })
-      .catch(err => console.log(err));
-  }
-  render() {
-    if (this.state.response === null) {
-      return <p>Loading data</p>
-    }
-    else {
-      var arrayOfData = this.state.response.map((item, index) => {
-        var path = '/'+item.id;
-        return (
-          
-          <div key={index}>
-
-            <p><Link to={path}>{item.id}</Link></p>
-          </div>
-        )
-       
-      });
-      console.log('arr: ', arrayOfData);
-     return  arrayOfData
-    }
-    
-  }
-  
-}
-
-
+import Products from './Product.js'
 
 class App extends Component {
   constructor(props) {
@@ -66,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ListOfProducts />
+        <Products />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Router, Route } from 'react-router';
-import { HashRouter } from 'react-router-dom';
+
 import Products from './Product.js'
 
-ReactDOM.render(
-    <HashRouter>
-        <div>
-        <Route path="/" component={App}/>
-    {/* add the routes here */}
-        <Route path="/1" component={Products}/>
-        <Route path="/2" component={Products}/>
-        </div>
-        
-    </HashRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
